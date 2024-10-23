b18a79e81c5e1c2e8a117fe796c70009482001e3
galaxygastronaut <90231794+icebear5h@users.noreply.github.com>
2024-10-23 17:36:43 -0400

    push to vercel deploy final

--- database.types.ts
+++
@@ -1,8 +0,0 @@
-[?25l
-    Select a project:                                                                       
-                                                                                            
-  >  1. oaybzdpqluokyllpjqyz [name: Maybility, org: hsdhdqxxxhyjidrnhfjj, region: us-west-1]
-                                                                                            
-                                                                                            
-    ↑/k up • ↓/j down • / filter • q quit • ? more                                          
-                                                                                            [0D[2K[?25h[?1002l[?1003l[?1006l
